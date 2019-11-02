ab0823cf5f343b4c3cee7452bbaed4caf8222c5e
Maks <nazirov.maksat96@gmail.com>
2019-11-02 10:28:14 +0600

    P4_t3

---
+++ task3.py
@@ -0,0 +1,23 @@
+# class Car:
+#     def __init__(self, make, model, year, odometer, fuel):
+       
+#         self.odometer = 0 
+#         self.fuel = 70 
+#     def __add_distance(self, km):
+#         self.odometer += km
+
+#     def __subtract_fuel(self, benzin):
+#         self.fuel -= benzin
+
+#     def drive(self, km):
+#         benzin = km/10
+#         if km/10 <= 70:
+#             self.__add_distance(km)
+#             self.__subtract_fuel(benzin)
+#             print("let's go")
+#         else:
+#             print('need more fuel, please, fill more!')
+
+# Audi = Car ('Audi', 'A6', 2014, 0, 70)
+# Audi.drive(700)
+   
